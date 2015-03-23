2bad2ff4f7343e05923024cf15aa54b676538477
Asain Kujovic <asainnp@gmail.com>
2015-03-23 16:43:01 +0100

    ...

--- routekeyskbd.sh
+++ routekeyskbd.sh
@@ -1,8 +1,7 @@
 #! /usr/bin/env bash
 
 cd /dev/input/by-path
-ls | grep mouse | grep event | xargs readlink | xargs realpath | sort -u
-#ls | grep 'kbd\|mouse' | grep event | xargs readlink | xargs realpath | sort -u
+ls | grep 'kbd\|mouse' | grep event | xargs -L1 readlink -f
 
 #'sort -u' used for unique list elements, more than one dev link can point
 #   to some /dev/input/eventX
